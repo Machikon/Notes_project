01dafa295eea6ed09317eac21b57ac721e5684f7
Machikon <106935371+Machikon@users.noreply.github.com>
2022-10-28 20:50:23 -0700

    update for heroku

--- routes/html.js
+++ routes/html.js
@@ -5,11 +5,11 @@ module.exports = app =>{
 
 // GET - Route for notes
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, '../public/notes.html'))
 );
 
 // GET - Route for index.html file
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, '../public/index.html'))
 );
 };
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const api = require('./routes/api.js');
 const html = require('./routes/html.js');
@@ -10,8 +10,6 @@ const html = require('./routes/html.js');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use('/api', api);
-
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
